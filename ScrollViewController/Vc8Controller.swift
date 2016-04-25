7d93ea46af5ca13f155a9a38d2c51709e4c43d52
ZeroJ <854136959@qq.com>
2016-04-25 19:00:06 +0800

    ...

--- ScrollViewController/Vc8Controller.swift
+++ ScrollViewController/Vc8Controller.swift
@@ -4,11 +4,29 @@
 //
 //  Created by jasnig on 16/4/21.
 //  Copyright © 2016年 ZeroJ. All rights reserved.
-//
-
 // github: https://github.com/jasnig
-// 简书: http://www.jianshu.com/p/b84f4dd96d0c
+// 简书: http://www.jianshu.com/users/fb31a3d1ec30/latest_articles
 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+//
 import UIKit
 
 /// 这些常量请根据项目需要修改
